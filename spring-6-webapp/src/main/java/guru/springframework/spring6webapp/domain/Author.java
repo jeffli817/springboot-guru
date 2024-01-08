796944b82b2961fb94a670fa57f97fe4ea0eaf32
jeffli <jeffli817@gmail.com>
2024-01-08 14:52:07 +0800

    20. Init Data With Spring

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -2,6 +2,8 @@ package guru.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -12,7 +14,7 @@ public class Author {
     private String firstName;
     private String lastName;
     @ManyToMany(mappedBy="authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Set<Book> getBooks() {
         return books;
@@ -40,6 +42,29 @@ public class Author {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id) && Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName) && Objects.equals(books, author.books);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, books);
     }
 
     public void setLastName(String lastName) {
