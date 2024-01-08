ddaef3ef84bfb36b1c6914e2720f256d59e2ec80
jeffli <jeffli817@gmail.com>
2024-01-08 17:47:18 +0800

    30. spring context

---
+++ spring-di/src/main/java/guru/springframework/springdi/SpringDiApplication.java
@@ -0,0 +1,22 @@
+package guru.springframework.springdi;
+
+import guru.springframework.springdi.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SpringDiApplication.class, args);
+
+		MyController controller = ctx.getBean(MyController.class);
+
+		System.out.println("In Main Method");
+
+		System.out.println(controller.sayHello());
+
+	}
+
+}
