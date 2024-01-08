12f657eec84907b54affcf7e17ac7763e073161b
jeffli <jeffli817@gmail.com>
2024-01-08 19:26:07 +0800

    37. Qualifier

--- spring-di/src/main/java/guru/springframework/springdi/controllers/PropertyInjectedController.java
+++ spring-di/src/main/java/guru/springframework/springdi/controllers/PropertyInjectedController.java
@@ -2,11 +2,13 @@ package guru.springframework.springdi.controllers;
 
 import guru.springframework.springdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
     @Autowired
+    @Qualifier("propertyGreetingService")
     GreetingService greetingService;
 
     public String sayHello(){
--- spring-di/src/main/java/guru/springframework/springdi/controllers/SettleInjectedController.java
+++ spring-di/src/main/java/guru/springframework/springdi/controllers/SettleInjectedController.java
@@ -2,12 +2,14 @@ package guru.springframework.springdi.controllers;
 
 import guru.springframework.springdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SettleInjectedController {
     private GreetingService greetingService;
 
+    @Qualifier("setterGreetingBean")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
