e15afa9b71137128f0257d37005056e5eb7a809d
jeffli <jeffli817@gmail.com>
2024-01-08 18:07:59 +0800

    33. Dependency without injection

---
+++ spring-di/src/main/java/guru/springframework/springdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,16 @@
+package guru.springframework.springdi.controllers;
+
+import guru.springframework.springdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
+}
