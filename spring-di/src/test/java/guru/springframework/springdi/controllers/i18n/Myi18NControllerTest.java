c2b4da35c513bf9ae14e67793b4710260557ce37
jeffli <jeffli817@gmail.com>
2024-01-08 19:36:54 +0800

    39. Profile

---
+++ spring-di/src/test/java/guru/springframework/springdi/controllers/i18n/Myi18NControllerTest.java
@@ -0,0 +1,48 @@
+package guru.springframework.springdi.controllers.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("EN")
+@SpringBootTest
+class myi18NControllerTestEN {
+
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello() {
+
+        System.out.println(myi18NController.sayHello());
+    }
+}
+
+@ActiveProfiles("ES")
+@SpringBootTest
+class myi18NControllerTestES {
+
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello() {
+        System.out.println(myi18NController.sayHello());
+    }
+}
+
+@SpringBootTest
+class myi18NControllerTestDefault {
+
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello() {
+        System.out.println(myi18NController.sayHello());
+    }
+}
+
